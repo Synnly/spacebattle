9c486bfc6faa64b9dd780f341922f4c88c31925b
Tristan Lagarde <lagarde48u@aig-ats-315-l11.fst.site.univ-lorraine.fr>
2022-03-24 17:39:11 +0100

    les commentaires vraiment !

--- tests/main.c
+++ tests/main.c
@@ -161,7 +161,7 @@ void handle_events(SDL_Event *event,world_t *world){
               if(event->key.keysym.sym== SDLK_DOWN){
                   world->sprite_y+=MOVING_STEP;
               }
-              if(event->key.keysym.sym== SDLK_ESCAPE){
+              if(event->key.keysym.sym== SDLK_ESCAPE){ // si on appuie sur la touche échap on ferme le jeu
                 world->gameover =1;
               }
 
